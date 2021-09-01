bd8b97bfcdd8d87be4cc09689361b90d2e93fd9f
ashuliko <89221375+ashuliko@users.noreply.github.com>
2021-09-01 22:16:51 +0300

    changes code

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,41 +1,39 @@
-import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 
 public class FirstTest {
     WebDriver wd;
 
     @BeforeClass
     public void setUp() {
-
         wd = new ChromeDriver();
         wd.manage().window().maximize();
-
-        wd.get("https://www.google.com/");
-
-        }
-
-    @Test
-    public void openGoogle (){
-
-        wd.findElement(By.name("q")).click();
-        wd.findElement(By.name("q")).clear();
-        wd.findElement(By.name("q")).sendKeys("WebDriver"+Keys.ENTER);
-
-        wd.quit();
+        wd.get("http://google.com/");
     }
 
     @Test
-    public void openGoogle2 (){
-
+    public void openGoogle() {
         wd.findElement(By.name("q")).click();
         wd.findElement(By.name("q")).clear();
-        wd.findElement(By.name("q")).sendKeys("WebDriver"+Keys.ENTER);
+        wd.findElement(By.name("q")).sendKeys("WebDriver" + Keys.ENTER);
+    }
 
+    @Test
+    public void openGoogle2() {
+        wd.findElement(By.name("q")).click();
+        wd.findElement(By.name("q")).clear();
+        wd.findElement(By.name("q")).sendKeys("Selenium" + Keys.ENTER);
     }
 
     @AfterClass
-
     public void tearDown() {
         wd.quit();
     }
+
 }
