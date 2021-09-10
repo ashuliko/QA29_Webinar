a7ada63f19c90c6dfb68f7d93bb5c292770669e2
ashuliko <89221375+ashuliko@users.noreply.github.com>
2021-09-10 18:11:06 +0300

    changes click and type

--- src/test/java/WikiTest.java
+++ src/test/java/WikiTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -7,28 +8,36 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class WikiTest {
+    WebDriver driver;
 
     @BeforeClass
-    public void setUp(){
-        WebDriver driver = new ChromeDriver();
+    public void setUp() {
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
-
+        driver.navigate().to("https://www.wikipedia.org/");
     }
-
 
     @Test
-    public void searchArticleTest(){
-
-
+    public void searchArticleTest() {
+        type(By.cssSelector("#searchInput"), "WebDriver");
+        click(By.cssSelector("[type='submit']"));
     }
 
+    private void type(By locator, String text) {
+        click(locator);
+        //click(By.cssSelector("#searchInput"));
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    private void click(By locator) {
+        driver.findElement(locator).click();
+    }
 
     @AfterClass
-    public void tearDown(){
-
+    public void tearDown() {
+        driver.quit();
     }
-
 
 }
